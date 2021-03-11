771d9ac67eb48eb66e85b2487f5679e300331602
dannyt <cobreti_@hotmail.com>
2021-03-10 20:52:51 -0500

    removed .js and .js.map files

--- Types.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var TYPES = {
-    Application: Symbol('IApplication')
-};
-exports.default = TYPES;
-//# sourceMappingURL=Types.js.map
--- main.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// import {app, BrowserWindow} from 'electron';
-var inversity_config_1 = require("./inversity.config");
-var Types_1 = require("./Types");
-var application = inversity_config_1.default.get(Types_1.default.Application);
-application.run();
-//# sourceMappingURL=main.js.map
