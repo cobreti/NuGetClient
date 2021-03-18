f1d6210770703a3c5951aedf264c8c52a87c531f
dannyt <cobreti_@hotmail.com>
2021-03-17 20:39:33 -0400

    changed nodejs unit tests to mocha and chai

--- src/Application.spec.ts
+++ src/Application.spec.ts
@@ -1,18 +1,21 @@
+import { expect } from 'chai';
+import { describe, it, beforeEach } from 'mocha';
+
 import {createContainer} from './inversity.config';
 import {IApplication, IApplicationServiceId} from './IApplication';
 import {Container} from 'inversify';
 
-
 describe('first test', () => {
   let container: Container;
-  beforeAll(() => {
+
+  beforeEach(() => {
     container = createContainer();
   });
 
   it('get application instance', () => {
-    expect(container).not.toBeNull();
-    let app = container.get<IApplication>(IApplicationServiceId);
-    expect(app).not.toBeNull();
+    expect(container).not.to.equal(null);
+    const app = container.get<IApplication>(IApplicationServiceId);
+    expect(app).not.to.equal(null);
   });
 });
 
