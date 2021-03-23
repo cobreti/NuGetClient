436f76f02a909daced4834e1b6419ac5143b5663
dannyt <cobreti_@hotmail.com>
2021-03-23 19:07:53 -0400

    moved main source under /main folder

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 // import {app, BrowserWindow} from 'electron';
-import {createContainer} from './src/inversity.config';
+import {createContainer} from './src/main/inversity.config';
 // import * as path from 'path';
 // import * as url from 'url';
-import {IApplication, IApplicationServiceId} from './src/IApplication';
+import {IApplication, IApplicationServiceId} from './src/main/IApplication';
 
 
 const container = createContainer();
